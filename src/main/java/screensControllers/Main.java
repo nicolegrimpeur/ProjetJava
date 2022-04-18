223eb1ffd20cb07f9d55aae9e26e598ef9e4e440
Nicolas Barrat <nicolas.barrat@student.junia.com>
2022-04-18 18:46:51 +0200

    Version pour compiler jar

--- src/main/java/screensControllers/Main.java
+++ src/main/java/screensControllers/Main.java
@@ -1,16 +1,15 @@
 package screensControllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
@@ -18,17 +17,26 @@ public class Main {
         System.exit(0);
     }
 
-    @FXML private MenuItem aboutBtn;
+    @FXML
+    private MenuItem aboutBtn;
 
-    public void about(ActionEvent event) {
+    public void about() throws IOException {
+        // on récupère le fichier de propriétés du projet (src/main/resources)
+        final Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("project.properties"));
+
+        // on crée un popup avec le numéro de version
         Stage dialog = new Stage();
         dialog.initModality(Modality.APPLICATION_MODAL);
         dialog.initOwner(aboutBtn.getParentPopup().getOwnerWindow());
+
+        // Voir https://formations.imt-atlantique.fr/bd_ihm/ihm/listeners/_index.fr.files/Les%20e%CC%81ve%CC%80nements%20%20en%20JavaFX.pdf
+        // pour les types de zones
         StackPane dialogStackPane = new StackPane();
-        dialogStackPane.getChildren().add(new Text("Version : 0.1"));
-        Scene dialogScene = new Scene(dialogStackPane, 300, 200);
+        dialogStackPane.getChildren().add(new Text("Version : " + properties.getProperty("version")));
+        Scene dialogScene = new Scene(dialogStackPane, 250, 150);
+        dialog.setTitle("A propos");
         dialog.setScene(dialogScene);
         dialog.show();
-
     }
 }
